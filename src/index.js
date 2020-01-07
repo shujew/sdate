be866ddc4042105362464c703028ac9b77bf24d3
Shuaib Jewon <shuaib.jewon@slader.com>
2020-01-06 20:19:10 -0500

    Initialization working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-console.log('Hello world!');
+/**
+ * Calculates the delta between the local computer time and the server time from worldtimeapi and
+ * stores it inside a cookie
+ */
+const initialize = () => {
+  /** @type {string} */
+  const cookieName = 'sdate-delta';
+  /** @type {number} */
+  const cookieExpDays = 7;
+
+  /**
+   * Sets a cookie on the page
+   * @param {string} name
+   * @param {string|number} value
+   * @param {number} days
+   * @param {string} path
+   */
+  const setCookie = (name, value, days, path = '/') => {
+    const expires = new Date(Date.now() + days * 864e5).toUTCString();
+    document.cookie = `${name}=${encodeURIComponent(value)};`
+      + `expires=${expires};`
+      + `path=${path};`;
+      // + 'SameSite=None';
+  };
+
+  /**
+   * Fetches server date from worldtimeapi and calls callback on parsed server date
+   * @param {Function} callback
+   */
+  const fetchServerDate = (callback) => {
+    const endpoint = 'http://worldtimeapi.org/api/timezone/Etc/UTC';
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+      if (req.readyState === 4 && req.status === 200) {
+        /** @type {string} */
+        const respText = (req.responseText && !!req.responseText.length) ? req.responseText : '{}';
+        /** @type {Object} */
+        const respJson = JSON.parse(respText);
+        /** @type {Object} */
+        const utcDateTime = ('datetime' in respJson) ? Date.parse(respJson.datetime) : Date.now();
+        callback(utcDateTime);
+      }
+    };
+    req.open('GET', endpoint, true);
+    req.send();
+  };
+
+  /**
+   * Callback for fetching server date
+   * @callback fetchServerDate
+   * @param {Date} serverDate
+   */
+  const parseServerDate = (serverDate) => {
+    const delta = serverDate - Date.now();
+    setCookie(cookieName, delta, cookieExpDays);
+  };
+
+  fetchServerDate(parseServerDate);
+};
+
+initialize();
